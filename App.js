5485f0b449e75806024067eabc8856f873c07c40
heowoong12 <81053210+heowoong12@users.noreply.github.com>
2023-08-23 10:50:01 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <input />
+        <input />
+        <input type="button" value="+" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
